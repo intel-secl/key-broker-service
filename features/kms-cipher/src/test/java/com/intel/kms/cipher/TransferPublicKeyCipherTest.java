d73a20355e3ddc99e5abcfa0f08f8a747820db0d
Rawat, Arvind <arvind.rawat@intel.com>
2019-08-06 06:54:36 -0700

    V1.6/copyright header

--- features/kms-cipher/src/test/java/com/intel/kms/cipher/TransferPublicKeyCipherTest.java
+++ features/kms-cipher/src/test/java/com/intel/kms/cipher/TransferPublicKeyCipherTest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2016 Intel Corporation. All rights reserved.
+ * Copyright (C) 2019 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 package com.intel.kms.cipher;
 
--- features/kms-keystore/src/main/java/com/intel/kms/console/cmd/CreateKey.java
+++ features/kms-keystore/src/main/java/com/intel/kms/console/cmd/CreateKey.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2012 Intel Corporation
- * All rights reserved.
+ * Copyright (C) 2019 Intel Corporation
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 package com.intel.kms.console.cmd;
 
